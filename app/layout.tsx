22736c278f864ebed886c9448e693bdf9c61d928
ham ⋆˙⟡♡ <133094508+harmonyeva@users.noreply.github.com>
2024-12-29 14:20:28 -0800

    updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Patrick_Hand } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const patrickHand = Patrick_Hand({
   weight: "400",
